dbd4c0eb5e346e96a238cb8ff523f3d18a8d2578
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-24 01:30:43 +0530

    Applying spectrum to array

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-const spectrum = ["#11FF00", "#B4FF24", "#E2FF47", "#FCFF2D", "#F1A72D", "#FF7438", "#FF8600", "#FF3A2A", "#FF0000"];
+const spectrum = ["#11FF00", "#B4FF24", "#E2FF47", "#FCFF2D", "#F1A72D", "#FF7438", "#FF531D", "#FF3A2A", "#FF0000"];
 
-const el = document.getElementsByClassName('el');
+const elementClass = document.getElementsByClassName('el') as HTMLCollectionOf<HTMLDivElement>;
+
+const el = Array.from(elementClass);
+
+el.forEach(element => {
+    let spectrumIndex: number = +element.textContent! - 1 | 0;
+    element.style.backgroundColor = spectrum[spectrumIndex];
+})
+
+function sort() {
+    return;
+}
