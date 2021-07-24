fdf1ba4fb0e7f872d77999ad440f4c65806251ef
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-24 22:22:56 +0530

    optimized bubble sort
    formatted code

--- main.ts
+++ main.ts
@@ -10,45 +10,49 @@ const spectrum = [
   "#FF0000",
 ];
 
-const visual = document.getElementById('visual') as HTMLDivElement;
+const visual = document.getElementById("visual") as HTMLDivElement;
 const sorts = document.getElementById("sorts");
 
 const elementClass = document.getElementsByClassName(
   "el"
 ) as HTMLCollectionOf<HTMLDivElement>;
 
-
 function snap(arr: number[]): void {
-  const arrayDiv = document.createElement('div');
-  arrayDiv.classList.add('array');
+  const arrayDiv = document.createElement("div");
+  arrayDiv.classList.add("array");
   visual.appendChild(arrayDiv);
 
-  arr.forEach(el => {
-    let element = document.createElement('div');
+  arr.forEach((el) => {
+    let element = document.createElement("div");
     element.textContent = `${el}`;
-    element.classList.add('el');
+    element.classList.add("el");
     element.style.backgroundColor = spectrum[el];
     arrayDiv.appendChild(element);
-  })
+  });
 }
 
 function bubbleSort(array: number[]) {
-  for(let i = 0; i < array.length - 1; i++) {
-    for(let j = 0; j < array.length - i - 1; j++) {
-      if(array[j] > array[j + 1]) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let swapped = false;
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
         const temp = array[j + 1];
         array[j + 1] = array[j];
         array[j] = temp;
+        swapped = true;
       }
       snap(array);
-    } 
-  } 
+    }
+    if (swapped == false) {
+      break;
+    }
+  }
 }
 
 function insertionSort(array: number[]) {
-  for(let i = 1; i < array.length; i++) {
+  for (let i = 1; i < array.length; i++) {
     let j = i;
-    while(j > 0 && (array[j] < array[j - 1] )) {
+    while (j > 0 && array[j] < array[j - 1]) {
       const temp = array[j - 1];
       array[j - 1] = array[j];
       array[j] = temp;
@@ -65,37 +69,41 @@ function formArray(mode: string) {
   array = shuffle(array);
   visual.innerHTML = "";
 
-  const arrayDiv =  document.querySelector('.array') as HTMLDivElement;
-  
-  array.forEach(element => {
+  const arrayDiv = document.querySelector(".array") as HTMLDivElement;
+
+  array.forEach((element) => {
     str += `<div class="el" style="background: ${spectrum[element]};">${element}</div>\n`;
-  })
-  arrayDiv.innerHTML = str; 
-  
-  if(mode === "bubble") {
+  });
+  arrayDiv.innerHTML = str;
+
+  if (mode === "bubble") {
     bubbleSort(array);
-  } else if (mode === "insertion"){
+  } else if (mode === "insertion") {
     insertionSort(array);
   } else {
-    return; 
+    return;
   }
 }
 
 function shuffle(array: number[]) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   while (0 !== currentIndex) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
 }
 
 function sort() {
-  const sortSelect = document.getElementById('sorts') as HTMLSelectElement;
+  const sortSelect = document.getElementById("sorts") as HTMLSelectElement;
   const sortType = sortSelect.value;
   formArray(sortType);
 }
