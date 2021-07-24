1c222d7dcba3d01a11625635900076415b213f3e
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-24 19:11:14 +0530

    forming array

--- main.ts
+++ main.ts
@@ -1,14 +1,38 @@
-const spectrum = ["#11FF00", "#B4FF24", "#E2FF47", "#FCFF2D", "#F1A72D", "#FF7438", "#FF531D", "#FF3A2A", "#FF0000"];
+const spectrum = [
+  "#11FF00",
+  "#B4FF24",
+  "#E2FF47",
+  "#FCFF2D",
+  "#F1A72D",
+  "#FF7438",
+  "#FF531D",
+  "#FF3A2A",
+  "#FF0000",
+];
 
-const elementClass = document.getElementsByClassName('el') as HTMLCollectionOf<HTMLDivElement>;
+const sorts = document.getElementById("sorts");
+
+const elementClass = document.getElementsByClassName(
+  "el"
+) as HTMLCollectionOf<HTMLDivElement>;
 
 const el = Array.from(elementClass);
 
-el.forEach(element => {
-    let spectrumIndex: number = +element.textContent! - 1 | 0;
+let array: number[] = [];
+
+function formArray(upperBound: number, lowerBound: number) {
+  for (let i = lowerBound; i < upperBound; i++) {
+    array.push(i);
+  }
+}
+
+function snap() {
+  el.forEach((element) => {
+    let spectrumIndex: number = (+element.textContent! - 1) | 0;
     element.style.backgroundColor = spectrum[spectrumIndex];
-})
+  });
+}
 
 function sort() {
-    return;
+  formArray(1, 10);
 }
